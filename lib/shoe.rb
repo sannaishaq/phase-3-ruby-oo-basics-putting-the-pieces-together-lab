9181892cb64ab951426a9d00bbb95cbe75126971
Sanna Ishaq <sakinah.ishaq@gmail.com>
2022-04-06 09:44:28 -0400

    commit tree 6d0ec510142984eb21dbb9b1fb5a77e4c1c7a2dc

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,1 +1,18 @@
-# Make your shoe class here!
+class Book
+
+    attr_accessor :brand, :color, :size, :material, :condition
+    def initialize(brand)
+        @brand = brand
+        @color = color
+        @size = size
+        @material = material
+        @condition = condition
+    end
+
+
+    def cobble
+        puts "Your shoe is as good as new!"
+        shoe.condition = "new"
+    end
+    
+end
